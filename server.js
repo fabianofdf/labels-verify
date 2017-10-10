2c19ab135234ce29abf7827fbe5a30f0ef4fafa2
Tiago Porto <tiago.porto@hotmart.com.br>
2017-10-10 17:55:54 -0300

    Auto open browser

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var host = "127.0.0.1";
 var port = 8080;
 var express = require("express");
 var main = require("./lib/main.js");
+var exec = require('child_process').exec
 
 var app = express();
 app.use(express.static(__dirname + "/app")); //use static files in ROOT/public folder
@@ -19,5 +20,5 @@ app.get("/analyse", function(req, res){
 
 app.listen(port, host);
 
-console.log("i18n Verify Server started!");
-console.log("Access: 'localhost:"+port+"'");
+exec('open http://localhost:' + port)
+console.log("i18n Verify Server running in localhost:"+port+"!");
