7d5c136eb7492dab474ac31f9496cb4f827262c5
Fabiano Fonseca de Freitas <fabianofdf@gmail.com>
2017-11-12 23:54:15 -0200

    Add form to configure paths

--- server.js
+++ server.js
@@ -3,18 +3,23 @@ var port = 8080;
 var express = require("express");
 var main = require("./lib/main.js");
 var exec = require('child_process').exec
+var bodyParser = require('body-parser');
 
 var app = express();
+
+app.use(bodyParser.json());
 app.use(express.static(__dirname + "/app")); //use static files in ROOT/public folder
 
-app.get("/analyse", function(req, res){
-  main.analyse()
+app.post("/analyse", function(req, res){
+  main.analyse(req.body.langPath, req.body.baseDirPath)
     .then(labels => {
       res.header("Content-Type", "text/json");
       res.send(labels);
     })
     .catch(err => {
-      res.send(err);
+      console.error(err.stack)
+      res.header("Content-Type", "text/plain");
+      res.status(500).send(err.stack)
     });
 });
 
