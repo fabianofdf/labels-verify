6a86a15ab318e2870b2ff30c507d581e22864772
Fabiano Fonseca de Freitas <fabianofdf@gmail.com>
2018-06-29 18:51:31 -0300

    Improvements

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
-var { analyse } = require("./lib/main.js");
-var express = require("express");
+var { analyse } = require("./lib/main.js")
+var express = require("express")
 var exec = require('child_process').exec
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser')
+const config = require('./config.json')
 
-var app = express();
-var host = "127.0.0.1";
-var port = 8080;
+var app = express()
+var host = "127.0.0.1"
+var port = 8080
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/app")); //use static files in ROOT/public folder
 
 app.post("/analyse", function(req, res){
   const { langPath, baseDirPath } = req.body
-  analyse(langPath, baseDirPath)
-    .then(labels => {
-      res.header("Content-Type", "text/json");
-      res.send(labels);
-    })
-    .catch(err => {
-      console.error(err.stack)
-      res.header("Content-Type", "text/plain");
-      res.status(500).send(err.stack)
-    });
+
+  try {
+    const labels = analyse(langPath, baseDirPath, config.ignoreLabels)
+    res.header("Content-Type", "text/json");
+    res.send(labels);
+  } catch (err) {
+    console.error(err.stack)
+    res.header("Content-Type", "text/plain");
+    res.status(500).send(err.stack)
+  }
 });
 
 app.listen(port, host);
